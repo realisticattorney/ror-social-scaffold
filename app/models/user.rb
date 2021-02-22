4aebedcfec783cf71d5e5cb0dbd394df2c8b9550
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-02-22 23:17:04 +0300

    add methods for friends request

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,18 +13,41 @@ class User < ApplicationRecord
 
   has_many :friends 
 
-  def list
-    list = []
+  def add_friends(user_friend , user)
+    user.friends.create(friend_id: user_friend.id)
+    user_friend.friends.create(friend_id: user.id)
+  end
+
+  def accept_request(user_friend)
+    friends.where(friend_id: user_friend.id).update(status: true).save!
+  end
+
+  def pending_list
+    pending_list = []
     friends.each do |f|
-     list << f.friend.name
+      if f.status == nil
+      pending_list << f.friend.name
+     end
     end
-    list
+     pending_list
+  end
+
+  def friends_list
+    friends_list = []
+    friends.each do |f|
+      if f.status == true
+    friends_list << f.friend.name
+      end
+    end
+    friends_list
   end
 
   def reverse_friends(u)
     u.friends.each do |f|
-      Friend.create(user_id: f.friend_id , friend_id: u.id )
+      Friend.create(user_id: f.friend_id , friend_id: u.id , status: true )
     end
   end
+
+ 
   
 end
