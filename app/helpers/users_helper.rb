d7ee137eea29471f872182cfb0f4610557fd610e
tavo <gasb_150@hotmail.com>
2021-02-26 11:19:16 -0500

    Use users_helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,11 +10,11 @@ module UsersHelper
         if user != current_user
             if user != nil
              if (!current_user.pending_list_show.include?(user) && !user.pending_list_show.include?(current_user))&& !current_user.friends_list.include?(user)
-                Friend.create(user_id: user.id, friend_id: current_user.id)
+                #Friend.create(user_id: user.id, friend_id: current_user.id)
 
 
                 content_tag(:div,(button_to 'Add',
-                           friends_path(params: { friend: { friend_id: user.id, user_id: current_user.id } }),
+                           friends_path(params: { friend: { user_id: user.id, friend_id: current_user.id } }),
                            method: :post, class: 'button-add'), class: 'button-add')
             end
         end
